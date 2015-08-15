8da1ce21466970afb7fe15923309f4fe91b5d71f
fand <fand@gmork.in>
2015-08-16 01:54:45 +0900

    fix rename script

--- script/rename.pl
+++ script/rename.pl
@@ -4,6 +4,7 @@ use warnings;
 use utf8;
 
 use Pod::Usage;
+use List::Util;
 
 my $after = $ARGV[0];
 pod2usage() unless $after;
@@ -16,43 +17,50 @@ sub rename_project {
     my $project_file = glob '*.xcodeproj';
     my $before = $project_file =~ s/(.*)\.xcodeproj/$1/r;
 
-    print "Rename the project from '$before' to '$after'.\n";
+    print "Rename the project from '$before' to '$after'.\n\n";
 
     rename_texts($before, $after);
+    rename_dirs($before, $after);
     rename_files($before, $after);
+
+    print "\ndone.\n";
 }
 
 sub rename_texts {
     my ($before, $after) = @_;
-    print "Rename the texts from '$before' to '$after'.\n";
+    print "Rename the texts...\n";
     system "git grep -l $before | xargs sed -i '' -e 's/$before/$after/g'";
+}
+
+sub rename_dirs {
+    my ($before, $after) = @_;
+    print "Rename the dirs...\n";
+
+    my $dirs_map = {};
+
+    my @paths = map { chomp; $_ }`git ls-files | grep '$before'`;
+    for my $path (@paths) {
+        if ($path =~ qr{.*$before[^/]*/.*}) {
+            my $dirname = $path =~ s{(.*$before[^/]*)/.*}{$1}r;
+            $dirs_map->{$dirname} = 1;
+        }
+    }
+
+    for my $dir (keys $dirs_map) {
+        my $newdir = $dir =~ s/$before/$after/rg;
+        print "\t'$dir' -> '$newdir'\n";
+        system "git mv $dir $newdir";
+    }
 }
 
 sub rename_files {
     my ($before, $after) = @_;
-    print "Rename the files from '$before' to '$after'.\n";
+    print "Rename the files...\n";
 
-    my @dirs = qw(
-        MyApp.xcodeproj
-        MyAppTests
-    );
-
-    for my $dir (@dirs) {
-        my $newdir = $dir =~ s/$before/$after/rg;
-        print "Renaming directory '$dir' to '$newdir' ...\n";
-        system "git mv $dir $newdir";
-    }
-
-    my @files = map {
-        $after . $_;
-    } qw(
-        .xcodeproj/xcshareddata/xcschemes/MyApp.xcscheme
-        Tests/MyAppTests.m
-    );
-
+    my @files = map { chomp; $_ } `git ls-files | grep '$before'`;
     for my $file (@files) {
         my $newfile = $file =~ s/$before/$after/rg;
-        print "Renaming file '$file' to '$newfile' ...\n";
+        print "\t'$file' -> '$newfile'\n";
         system "git mv $file $newfile";
     }
 }
